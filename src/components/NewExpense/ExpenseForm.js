c9f9142c3f349dd3d6793a03811500c6f1e6e26e
ali.abukahil <ali.abukahil@gmail.com>
2021-07-14 17:18:40 +0200

    The two way Binding to clear the input of the user after he/she submits

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -64,12 +64,33 @@ function ExpenseForm() {
     // });
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+    // The two way Binding to clear the input of the user after he/she submits
+    // Step 1
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+    // step 2 you add a value into the input you wanna reset after a submission Example: value={enteredTitle}
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -77,6 +98,7 @@ function ExpenseForm() {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -86,6 +108,7 @@ function ExpenseForm() {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
